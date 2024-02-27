3d86c7e29c41cb11092a9cd5db4a841267e978a1
Ermek Orazbekov <154459511+surf100@users.noreply.github.com>
2024-02-27 15:24:43 +0600

    Update BookServiceInterface.java

--- src/main/java/kz/aitu/librarysecond/services/interfaces/BookServiceInterface.java
+++ src/main/java/kz/aitu/librarysecond/services/interfaces/BookServiceInterface.java
@@ -2,9 +2,11 @@ package kz.aitu.librarysecond.services.interfaces;
 
 import kz.aitu.librarysecond.models.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookServiceInterface {
     List<Book> getAllBooks();
-    List<Book> getByNOR(int numberOfReaders);
+    List<Book> takeBook(int number);
+    List<Book> buyBook(int number);
 }
 
