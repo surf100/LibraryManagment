b0324c928a85f201f050dd79c383b5fcaef28758
Ermek Orazbekov <154459511+surf100@users.noreply.github.com>
2024-02-28 07:32:16 +0600

    Update BookService.java

--- src/main/java/kz/aitu/librarysecond/services/BookService.java
+++ src/main/java/kz/aitu/librarysecond/services/BookService.java
@@ -1,17 +1,24 @@
 package kz.aitu.librarysecond.services;
 
 import kz.aitu.librarysecond.models.Book;
+import kz.aitu.librarysecond.models.User;
 import kz.aitu.librarysecond.repositories.BookRepositoryInterface;
+import kz.aitu.librarysecond.repositories.UserRepositoryInterface;
 import kz.aitu.librarysecond.services.interfaces.BookServiceInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService implements BookServiceInterface {
 
     private final BookRepositoryInterface bookRepository;
-    public BookService(BookRepositoryInterface bookRepository) {
+    private final UserRepositoryInterface userRepository;
+    public BookService(BookRepositoryInterface bookRepository, UserRepositoryInterface userRepository) {
         this.bookRepository = bookRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -35,18 +42,9 @@ public class BookService implements BookServiceInterface {
     }
 
     @Override
-    public List<Book> buyBook(int number) {
-        List<Book> books = bookRepository.findByNumber(number);
-        if (!books.isEmpty()) {
-            Book book = books.get(0);
-            if(!book.isHas_price()){
-                return null;
-            }
-            book.setReaders(book.getReaders() + 1);
-            bookRepository.save(book);
-            return books;
-        }
-        return null;
+    public Book create(Book book) {
+        return bookRepository.save(book);
     }
+
 
 }
