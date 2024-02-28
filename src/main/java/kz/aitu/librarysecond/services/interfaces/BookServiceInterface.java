0247a0e18d499a817186a3355ed6fc833d435481
Ermek Orazbekov <154459511+surf100@users.noreply.github.com>
2024-02-28 07:31:24 +0600

    Update BookServiceInterface.java

--- src/main/java/kz/aitu/librarysecond/services/interfaces/BookServiceInterface.java
+++ src/main/java/kz/aitu/librarysecond/services/interfaces/BookServiceInterface.java
@@ -1,12 +1,14 @@
 package kz.aitu.librarysecond.services.interfaces;
 
 import kz.aitu.librarysecond.models.Book;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface BookServiceInterface {
     List<Book> getAllBooks();
     List<Book> takeBook(int number);
-    List<Book> buyBook(int number);
+    Book create(Book book);
 }
 
