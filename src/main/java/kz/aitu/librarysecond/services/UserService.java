de7230981003a0c4385cb72ceecb8001d3cd5f35
Ermek Orazbekov <154459511+surf100@users.noreply.github.com>
2024-03-01 23:25:28 +0600

    Update UserService.java

--- src/main/java/kz/aitu/librarysecond/services/UserService.java
+++ src/main/java/kz/aitu/librarysecond/services/UserService.java
@@ -1,5 +1,6 @@
 package kz.aitu.librarysecond.services;
 
+import kz.aitu.librarysecond.models.Book;
 import kz.aitu.librarysecond.models.User;
 import kz.aitu.librarysecond.repositories.UserRepositoryInterface;
 import kz.aitu.librarysecond.services.interfaces.UserServiceInterface;
@@ -10,44 +11,43 @@ import java.util.Optional;
 
 @Service
 public class UserService implements UserServiceInterface {
-    public UserRepositoryInterface repo;
+    public UserRepositoryInterface userRepository;
 
     public UserService(UserRepositoryInterface repo) {
-        this.repo = repo;
+        this.userRepository = repo;
     }
 
     @Override
     public List<User> getAllUser() {
-        return repo.findAll();
+        return userRepository.findAll();
     }
 
     @Override
     public User create(User user) {
-        return repo.save(user);
-    }
-    
-    @Override
-    public List<User> getBySurname(String surname) {
-        return repo.findBySurname(surname);
+        return userRepository.save(user);
     }
 
     @Override
-    public void topUpBalance(int userId, float amount) {
-    User user  = userRepository.findById(userId).stream().findFirst().orElse(null);    
-        user.setBalance(user.getBalance()+amount);
-        userRepository.save(user);
+    public List<User> getBySurname(String surname) {
+        return userRepository.findBySurname(surname);
     }
-    
+
     @Override
     public User getUserByEmail(String email) {
-        return repo.findByEmail(email);
+        return userRepository.findByEmail(email);
     }
 
     @Override
     public float getUserBalanceById(int userId) {
-    User user  = userRepository.findById(userId).stream().findFirst().orElse(null);
-    return user.getBalance();
-}
-    
+        User user  = userRepository.findById(userId).stream().findFirst().orElse(null);
+        return user.getBalance();
     }
+    @Override
+    public void topUpBalance(int userId, float amount) {
+        User user  = userRepository.findById(userId).stream().findFirst().orElse(null);
+        user.setBalance(user.getBalance()+amount);
+        userRepository.save(user);
+    }
+
+
 }
